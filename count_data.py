74077c58f8a736b4feabf331e114201258d68358
wmccrthy <wyattmccarthy2020@gmail.com>
2024-10-13 17:41:47 -0400

    Slightly modified file to gather some analytics

--- count_data.py
+++ count_data.py
@@ -5,13 +5,13 @@ from Bio.Seq import Seq
 SIMPLE SCRIPT FOR COUNTING THE DATA COLLECTED SO FAR (USEFUL UTILITY)
 """
 
-data_path_MIT = f"/Mounts/rbg-storage1/users/wmccrthy/dataSubSets/" 
+data_path_MIT = f"/data/rbg/users/wmccrthy/rbgquanta1/wmccrthy/" 
 total_data = 0
-total_data_w_seq = 0 
+total_data_w_seq = 0
 
-for file in os.listdir(data_path_MIT):
-    file_path = data_path_MIT + file
-    if file[:3] == "API": continue 
+def count_seqs(file_name):
+    file_path = data_path_MIT + file_name
+    print(file_path)
     with open(file_path) as read_from:
         for line in read_from:
             line = line.split(",")
@@ -19,5 +19,13 @@ for file in os.listdir(data_path_MIT):
                 total_data += 1
                 if len(line[-1]) > 50: total_data_w_seq += 1
 
-print("Total Data Points:", total_data)
-print("Total Data Points w (not null) Sequences:",total_data_w_seq)
+    print("Total Data Points:", total_data)
+    print("Total Data Points w (not null) Sequences:",total_data_w_seq)
+
+if __name__ == "__main__":
+    args = sys.argv
+    print(args)
+    # args[0] = current file
+    # args[1] = function name
+    # args[2:] = function args : (*unpacked)
+    globals()[args[1]](*args[2:])

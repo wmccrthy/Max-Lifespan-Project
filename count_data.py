3c08365cb8500575569515165004809023dfebd3
wmccrthy <wyattmccarthy2020@gmail.com>
2024-10-14 19:47:35 -0400

    fixed counting script

--- count_data.py
+++ count_data.py
@@ -1,21 +1,18 @@
-import os, sys, Bio, csv 
-from Bio.Seq import Seq
+import os, sys, csv 
 
 """
 SIMPLE SCRIPT FOR COUNTING THE DATA COLLECTED SO FAR (USEFUL UTILITY)
 """
-
-data_path_MIT = f"/data/rbg/users/wmccrthy/rbgquanta1/wmccrthy/" 
-total_data = 0
-total_data_w_seq = 0
-
 def count_seqs(file_name):
+    data_path_MIT = f"/data/rbg/users/wmccrthy/rbgquanta1/wmccrthy/" 
+    total_data = 0
+    total_data_w_seq = 0
     file_path = data_path_MIT + file_name
     print(file_path)
     with open(file_path) as read_from:
         for line in read_from:
             line = line.split(",")
-            if len(line) == 10: 
+            if len(line) == 9: 
                 total_data += 1
                 if len(line[-1]) > 50: total_data_w_seq += 1
 

ac661da4dd42ae1b21cb9b0f7e6e7f2600835606
wmccrthy <wyattmccarthy2020@gmail.com>
2024-10-18 18:21:48 -0400

    new EDA to collect metrics on one2one gene datasets

--- EDA/trim_data.py
+++ EDA/trim_data.py
@@ -25,12 +25,14 @@ BROAD SCRIPT FOR THE PURPOSE OF TRIMMING DATA
             - between lengths of 104 and 31620
 
 """
-import os, csv, sys
+import os, csv, sys, numpy as np
+import matplotlib.pyplot as plt
 from collections import Counter
 
 cumulative_untrimmed_path = "/data/rsg/chemistry/wmccrthy/Everything/cumulativeTOGAset.csv"
 
-gene_datasets_path = "/data/rsg/chemistry/wmccrthy/Everything/gene_datasets/regulatory/"
+gene_datasets_path = "/data/rbg/users/wmccrthy/chemistry/Everything/gene_datasets/regulatory/"
+gene_one2one_datasets_path = "/data/rbg/users/wmccrthy/chemistry/Everything/gene_datasets/regulatory_one2one/"
 
 
 invalid_intactness = set(["lost", "missing", "paralogous projection"])
@@ -78,6 +80,135 @@ def trim_cumulative():
                 elif Counter(line[-1])['-'] > len(line[-1]) / 5: continue #if too many gaps in sequence, don't output 
                 writer.writerow(line)
 
+"""
+method to trim a given gene's csv file path (expected format: ['organism','max_lifespan', 'gene_id','orthologType','chromosome','start','end','direction (+/-)','intactness','sequence'])
+generates new file only including one2one sequences btwn lengths of 104 and 31620 (put in new directory for organization's sake)
+also computes stats of # of sequences and # of species represented per gene (we can create histogram of these...)
+"""
+def create_one2one_gene_sets():
+    gene_stats = {} #dict to hold gene:(num_data_entries, num_species) pairs
+    num_genes = 0
+    for file in os.listdir(gene_datasets_path):
+        file_path = gene_datasets_path + file 
+        new_file_path = "/".join(file_path.split("/")[:-2]) + "/regulatory_one2one/" + "".join(file.split(".")[:-1]) + "_one2one.csv"
+        #only execute on most up-to-date 'trimmed' gene files 
+        if file_path[-21:] != "orthologs_trimmed.csv": continue
+        num_genes += 1
+        cur_gene = file.split("_")[0]
+        num_data = 0
+        organisms = set()
+
+        # print(new_file_path, cur_gene)
+
+        # continue
+
+        with open(new_file_path, "w") as write_to:
+            writer = csv.writer(write_to)
+            writer.writerow(['organism','max_lifespan', 'gene_id','orthologType','chromosome','start','end','direction (+/-)','intactness','sequence'])
+            with open(file_path) as read_from:
+                for line in read_from:
+                    line = line.split(",")
+                    if len(line) < 10: continue 
+                    seq = line[-1].strip()
+                    length = len(seq)
+                    if length < 104 or length > 31620: continue #only include seqs of length in this range
+                    ortholog_type = line[3]
+                    if ortholog_type != "one2one": continue #only include one2one seqs 
+                    num_data += 1
+                    print(line[0])
+                    organisms.add(line[0])
+                    writer.writerow(line)
+
+        gene_stats[cur_gene] = (num_data, len(organisms)) #fill dict entry for current gene
+
+        write_to.close()
+        os.system(f'rm -rf {file_path}')
+
+    # after iterating thru all genes s.t we've generated one2one, trimmed, gene sets
+    # create new file that outputs stats per gene (csv of format [gene | num_entries | num_species])
+    # we can use this file to generate histograms thereafter
+    
+    with open("/data/rbg/users/wmccrthy/chemistry/Everything/EDA/regulatory_one2one_sets_metadata.csv", "w") as write_to:
+        writer = csv.writer(write_to)
+        writer.writerow(["gene", "# seqs", "# species"])
+        for gene in gene_stats:
+            num_seqs, num_species = gene_stats[gene]
+            writer.writerow([gene, num_seqs, num_species])
+    write_to.close()
+
+
+"""
+Method that iterates through one2one gene datasets and outputs csv file with [gene, num sequences, num species]
+"""
+def get_one2one_stats():
+    gene_stats = {} #dict to hold gene:(num_data_entries, num_species) pairs
+    # after iterating thru all genes s.t we've generated one2one, trimmed, gene sets
+    # create new file that outputs stats per gene (csv of format [gene | num_entries | num_species])
+    # we can use this file to generate histograms thereafter
+    for file in os.listdir(gene_one2one_datasets_path):
+        file_path = gene_one2one_datasets_path + file
+        cur_gene = file.split("_")[0]
+        organisms = set()
+        num_data = 0
+        with open(file_path) as read_from:
+            for line in read_from:
+                line = line.split(",")
+                if len(line) < 10: continue #don't read empty lines
+                org = "_".join(line[0].split("_")[:2])
+                # print(org)
+                organisms.add(org)
+                num_data += 1
+        gene_stats[cur_gene] = (num_data, len(organisms))
+        # print(cur_gene, num_data, len(organisms))
+        if len(organisms) > 450:
+            print("============= WARNING =============")
+            print(len(organisms), "> total species")
+
+    with open("/data/rbg/users/wmccrthy/chemistry/Everything/EDA/regulatory_one2one_sets_metadata.csv", "w") as write_to:
+        writer = csv.writer(write_to)
+        writer.writerow(["gene", "# seqs", "# species"])
+        for gene in gene_stats:
+            num_seqs, num_species = gene_stats[gene]
+            writer.writerow([gene, num_seqs, num_species])
+    write_to.close()
+
+
+"""
+Iterate through regulatory_one2one_sets_metadata.csv and create histograms for each:
+    - distribution of # sequences per gene
+    - distribution of # species per gene
+"""
+def gene_one2one_histograms():
+    data_path = "/data/rbg/users/wmccrthy/chemistry/Everything/EDA/regulatory_one2one_sets_metadata.csv"
+    num_seqs = []
+    num_species = []
+    with open(data_path) as read_from:
+        for line in read_from:
+            line = line.split(",")
+            if len(line) < 3: continue #avoid janky csv parsing stuff
+            seqs, species = int(line[1].strip()), int(line[2].strip())
+            num_seqs.append(seqs)
+            num_species.append(species)
+    
+    num_seqs = np.array(num_seqs)
+    num_species = np.array(num_species)
+    # Generate the histogram
+    plt.figure(figsize=(10, 6))
+    plt.hist(num_seqs, bins=25, edgecolor='black')  # Adjust bins for better resolution
+    plt.title('Histogram of # Seqs per Gene')
+    plt.xlabel('# Seqs')
+    plt.ylabel('# Genes')
+    plt.grid(True)
+    plt.show()
+    plt.hist(num_species, bins=25, edgecolor='black')  # Adjust bins for better resolution
+    plt.title('Histogram of # Species per Gene')
+    plt.xlabel('# Species')
+    plt.ylabel('# Genes')
+    plt.grid(True)
+    plt.show()
+
+
+
 def trim_gene_sets():
     for file in os.listdir(gene_datasets_path):
         file_path = gene_datasets_path + file 
@@ -96,7 +227,8 @@ def trim_gene_sets():
                     if len(line) < 10: continue 
                     intactness = line[-2]
                     if intactness in invalid_intactness: continue #if missing or lost, don't output 
-                    elif Counter(line[-1])['-'] > len(line[-1]) / 5: continue #if too many gaps in sequence, don't output 
+                    elif Counter(line[-1])['-'] > len(line[-1]) / 5: continue #if too many gaps in sequence, don't output
+
         
                     writer.writerow(line)
         write_to.close()

c4b9a19e57bee35e2a30a012f25d6298f751b8b6
wmccrthy <wyattmccarthy2020@gmail.com>
2024-11-10 11:17:52 -0500

    script for organizing training results

---
+++ EDA/compile_per_gene_results.py
@@ -0,0 +1,53 @@
+import os, csv, sys
+
+
+"""
+Script for iterating through training results for per-gene enformer approach
+Loops through directories (corresponding to genes in: 
+/data/rbg/users/wmccrthy/chemistry/Everything/fall_24_training/gene_training_metrics/
+
+
+"""
+
+RESULTS_DIR = "/data/rbg/users/wmccrthy/chemistry/Everything/fall_24_training/gene_training_metrics/"
+TRAINING_DIR = "/data/rbg/users/wmccrthy/chemistry/Everything/fall_24_training/"
+
+"""
+Given a gene's training results directory, scrapes the metric file to return:
+    avg train loss, avg val loss, min val loss
+"""
+def scrape_metrics(path):
+    train_losses, val_losses = [], []
+    i = 0
+    val_min = float("inf")
+    with open(path + "/version_0/metrics.csv") as read_from:
+        for line in read_from:
+            line = line.split(",")
+            # for now we don't care abt epochs, we care abt averaging train_loss and val_loss
+            train_loss, val_loss = line[2:]
+            if i > 0:
+                if len(train_loss) > 0: train_losses.append(float(train_loss))
+                elif len(val_loss) > 0: val_losses.append(float(val_loss))
+            i += 1
+    train_loss = sum(train_losses)/len(train_losses)
+    val_loss = sum(val_losses)/len(val_losses)
+    val_min = min(val_losses)
+    return train_loss, val_loss, val_min
+
+"""
+
+"""
+with open(TRAINING_DIR + "per_gene_results.csv", "w") as write_to:
+    writer = csv.writer(write_to)
+    writer.writerow(["gene", "avg train loss", "avg val loss", "min val loss"])
+    for sub_dir in os.listdir(RESULTS_DIR):
+        gene = sub_dir.split("_")[0]
+        avg_train, avg_val, val_min = scrape_metrics(RESULTS_DIR + "/" + sub_dir)
+        print(gene, avg_train, avg_val, val_min)
+        writer.writerow([gene, avg_train, avg_val, val_min])
+
+
+
+
+            
+
